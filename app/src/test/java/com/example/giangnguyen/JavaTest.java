0111886f7532dcf248351ad50ffab3f8279e06ad
Glynn Nguyen <glynn.nguyen.goldenowl@gmail.com>
2023-10-25 14:58:17 +0700

    remove unit test

--- app/src/test/java/com/example/giangnguyen/JavaTest.java
+++ app/src/test/java/com/example/giangnguyen/JavaTest.java
@@ -1,15 +1,10 @@
 package com.example.giangnguyen;
 
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class JavaTest {
 
-    @Test
-    void testFuntion(){
-        int a = (int)888.8;
-        long xyz = (byte)100;
-        int t = (byte)100L;
-        byte t1 = (byte)100L;
-    }
 
 }
